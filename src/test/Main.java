4b3b8f4169d04cff8c397de48b60b04aef620570
Amanda Luz <alc2@cesar.school>
2025-09-15 18:52:18 -0300

    commit inicial

---
+++ src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		testinho a = new testinho();
+		a.setFodasse(0);
+		System.out.println(a.getFodasse());
+	}
+
+}
---
+++ src/test/testinho.java
@@ -0,0 +1,9 @@
+package test;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class testinho {
+	private double kikuLindo;
+}

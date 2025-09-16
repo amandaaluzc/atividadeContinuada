ecf8d63d6293800518cdece80f7b9d55ac05f953
Amanda Luz <alc2@cesar.school>
2025-09-15 22:00:25 -0300

    :sparkles: feat: implementar enuns 'TipoOrdem', 'Dificuldade' e 'PrecoBase'

--- src/br/edu/cs/poo/ac/ordem/entidades/Dificuldade.java
+++ src/br/edu/cs/poo/ac/ordem/entidades/Dificuldade.java
@@ -1,5 +1,30 @@
 package br.edu.cs.poo.ac.ordem.entidades;
 
 public enum Dificuldade {
-
+	NORMAL(1, "Normal"),
+	DIFICIL(2, "Difícil");
+	
+	private final int codigo;
+	private final String nome;
+	
+	private Dificuldade(int codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public static Dificuldade getDificuldade(int codigo) {
+		for (Dificuldade tipo : Dificuldade.values()) {
+			if (codigo == tipo.getCodigo()) {
+				return tipo;
+			}
+		}
+		return null;
+	}
 }

a0836934d6006d59e7607175dd74ea8c259147c7
Lucasmenezes08 <lms4@cesar.school>
2025-09-23 20:41:41 -0300

    :recycle: refactor: Refatorar Arquivos de teste.

--- src/br/edu/cs/poo/ac/ordem/daos/DAOGenerico.java
+++ src/br/edu/cs/poo/ac/ordem/daos/DAOGenerico.java
@@ -1,5 +1,10 @@
 package br.edu.cs.poo.ac.ordem.daos;
 
-public class DAOGenerico {
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
+public class DAOGenerico {
+	protected CadastroObjetos cadastroObjetos;
+	public DAOGenerico(Class classe) {
+		cadastroObjetos = new CadastroObjetos(classe);
+	}
 }

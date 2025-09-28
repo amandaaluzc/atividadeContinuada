27315de92068d86d15eb563e86a3764d67f94cd0
Lucasmenezes08 <lms4@cesar.school>
2025-09-28 19:49:57 -0300

    :sparkles: feat: Implementar FechamentoordemServicoDAO.

--- src/br/edu/cs/poo/ac/ordem/daos/DesktopDAO.java
+++ src/br/edu/cs/poo/ac/ordem/daos/DesktopDAO.java
@@ -1,6 +1,13 @@
 package br.edu.cs.poo.ac.ordem.daos;
+import java.io.Serializable;
+
+import br.edu.cs.poo.ac.ordem.entidades.Desktop;
 //O identificador único, por objeto, de Desktop é a concatenação do retorno 
 //do método getTipo com o atributo serial.   
-public class DesktopDAO {
 
+
+public class DesktopDAO extends DAOGenerico {
+	public DesktopDAO () {
+		super(Desktop.class);
+	}
 }
--- src/br/edu/cs/poo/ac/ordem/daos/FechamentoOrdemServicoDAO.java
+++ src/br/edu/cs/poo/ac/ordem/daos/FechamentoOrdemServicoDAO.java
@@ -1,6 +1,64 @@
 package br.edu.cs.poo.ac.ordem.daos;
+import java.io.Serializable;
+
+import br.edu.cs.poo.ac.ordem.entidades.FechamentoOrdemServico;
 //O identificador único, por objeto, de FechamentoOrdemServico 
 //é o número da ordem de serviço.   
-public class FechamentoOrdemServicoDAO {
 
+public class FechamentoOrdemServicoDAO extends DAOGenerico {
+		
+	public FechamentoOrdemServicoDAO(){
+		super(FechamentoOrdemServico.class);
+	}
+	
+	public FechamentoOrdemServico buscar(String numeroOrdemServico) {
+		return (FechamentoOrdemServico)cadastroObjetos.buscar(numeroOrdemServico);
+	}
+	
+	public boolean incluir(FechamentoOrdemServico fechamento) {
+		if (buscar(fechamento.getNumeroOrdemServico()) == null) {
+			cadastroObjetos.incluir(fechamento, fechamento.getNumeroOrdemServico());
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	public boolean alterar(FechamentoOrdemServico fechamento) {
+		if (buscar(fechamento.getNumeroOrdemServico()) != null) {
+			cadastroObjetos.alterar(fechamento, fechamento.getNumeroOrdemServico());
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean excluir(String numeroOrdem) {
+		if (buscar(numeroOrdem) != null) {
+			cadastroObjetos.excluir(numeroOrdem);
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	public FechamentoOrdemServico[] buscarTodos() {
+		Serializable[] ret = cadastroObjetos.buscarTodos();
+		FechamentoOrdemServico[] retorno;
+		if (ret != null && ret.length > 0) {
+			retorno = new FechamentoOrdemServico[ret.length];
+			for (int i = 0 ; i < ret.length ; i ++) {
+				retorno[i] = (FechamentoOrdemServico) ret[i];
+			}
+		}
+		else {
+			retorno = new FechamentoOrdemServico[0];
+		}
+		return retorno;
+	}
 }

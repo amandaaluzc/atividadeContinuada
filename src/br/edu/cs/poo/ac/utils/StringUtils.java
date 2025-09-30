742ad314f73472b68c1590da125da3448180aa3b
Lucasmenezes08 <lms4@cesar.school>
2025-09-30 18:56:31 -0300

    :test_tube: Test: implemeentar testes para stringUtils.

---
+++ src/br/edu/cs/poo/ac/utils/StringUtils.java
@@ -0,0 +1,77 @@
+package br.edu.cs.poo.ac.utils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+	public static boolean estaVazia(String str) {
+		if (str == null || str.trim().equals("")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean tamanhoExcedido(String str, int tamanho) {
+		if (tamanho > 0) {
+			if (str == null || str.trim().length() > tamanho) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean emailValido(String email) {
+		
+		if (email == null || email.trim().equals("")) {
+			return false;
+		}
+		
+		else {
+			String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+			
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(email);
+			
+			if (matcher.matches()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}	
+	}
+	
+	public static boolean telefoneValido(String tel) {
+		if (tel == null || tel.trim().equals("")) {
+			return false;
+		}
+		
+		else {
+			String digitos = tel.replaceAll("[^0-9]","");
+			
+			if (digitos.length() == 11 || digitos.length() == 10 ) {
+				String regex = "^\\(\\d{2}\\)\\d{8,9}$";
+				
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(tel);
+				
+				if (matcher.matches()) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+			
+		}
+	}
+}
